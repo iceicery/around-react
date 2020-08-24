2f913cc4ac67cf28c693ca7a9ecff5428a6c954a
Pinwei Wu <pw.amy.wu@gmail.com>
2020-08-24 15:53:16 -0500

    fix: add hidden to className instead of rendering popup component

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function PopupWithForm({ isOpen, onClose, onSubmit, name, title, buttonText, children }) {
+    const popupClassName=(`popup popup_type_${name} ${!isOpen && 'hidden'}`);
+    const darkClassName=(`${isOpen && 'darken'}`);
     return (
-        isOpen &&
         <>
-            <div className="darken"></div>
-            <section className={`popup popup_type_${name}`}>
+            <div className={darkClassName}></div>
+            <section className={popupClassName}>
                 <button className={`popup__button-icon popup__button-icon_type_${name}`} onClick={onClose}></button>
                 <form className={`popup__form popup__form_type_${name}`} noValidate onSubmit={onSubmit}>
                     <h2 className={`popup__title popup__title_type_${name}`}>{title}</h2>
